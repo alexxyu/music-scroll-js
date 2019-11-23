d329da923d8800a2a99664048524c49bfa9ed318
alexxyu <yu.alexanderx@gmail.com>
2019-11-23 08:52:48 -0800

    Fixed script issue

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.get('/opencv.js', function(request, response) {
+    res.sendFile(__dirname + '/node_modules/opencv.js/opencv.js');
+});
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 });
 
 console.log('App is running!');
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode.", this.address().port, app.settings.env);
 });
